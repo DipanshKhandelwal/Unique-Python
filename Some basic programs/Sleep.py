9e41583983830bfda34c813728ce47d1791348f8
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-12 04:02:27 +0530

    Added sleep and webbrowser programs

---
+++ Some basic programs/Sleep.py
@@ -0,0 +1,6 @@
+import time
+
+#program waits for 5 seconds before going further in the code lines
+print "Started"
+time.sleep(5)
+print "Stopped after 5 seconds"
--- Some basic programs/WebBrowser.py
+++ Some basic programs/WebBrowser.py
@@ -1,3 +1,4 @@
 import webbrowser 
 
+#python program opens a web browser window to the below written link
 webbrowser.open("https://github.com/DipanshKhandelwal")
