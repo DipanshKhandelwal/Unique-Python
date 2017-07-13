ef1425812e293b8944a3bf6abeead730c45cd692
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-14 04:38:42 +0530

    Added a method

--- New folder/entertainment_center.py
+++ New folder/entertainment_center.py
@@ -1,4 +1,12 @@
 import media
 
-toy_story = media.Movie("toy_story","boy and toy","link","trailer")
-avatar = media.Movie("avatar","marine on alien planet","link","trailer")
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
--- New folder/media.py
+++ New folder/media.py
@@ -5,3 +5,6 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
