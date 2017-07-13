662e6ccedc5dceb7d2fb7882bfbf3a200b48baf0
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-14 02:32:57 +0530

    Added program to read files and print

---
+++ PythonFiles/read_file/read_file.py
@@ -0,0 +1,7 @@
+def read_file():
+    content = open("C:/Users/DIPANSH KHANDELWAL/Desktop/Python Codes/PythonFiles/read_file/read_file.txt")
+    text = content.read()
+    print (text)
+    content.close()
+
+read_file()
