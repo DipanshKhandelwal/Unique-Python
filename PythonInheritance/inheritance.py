e12f5b1a099d0bcd550a053d34443fcf7f53a94d
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-15 01:50:39 +0530

    Added Child class

--- PythonInheritance/inheritance.py
+++ PythonInheritance/inheritance.py
@@ -4,6 +4,17 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child constructor called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
 billy_cyrus = Parent("Cyrus", "blue")
+
+miley_cyrus = Child("Cyrus" , "blue" , 5)
+
 print(billy_cyrus.last_name)
 
+print(miley_cyrus.last_name)
+print(miley_cyrus.number_of_toys)
