6e5c3d91b495906baad297830ac9f7dd132cd5aa
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-15 01:55:56 +0530

    Added new method show_info

--- PythonInheritance/inheritance.py
+++ PythonInheritance/inheritance.py
@@ -4,6 +4,10 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructor called")
@@ -11,10 +15,8 @@ class Child(Parent):
         self.number_of_toys = number_of_toys
 
 billy_cyrus = Parent("Cyrus", "blue")
-
 miley_cyrus = Child("Cyrus" , "blue" , 5)
 
-print(billy_cyrus.last_name)
+billy_cyrus.show_info()
+miley_cyrus.show_info()
 
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_of_toys)
