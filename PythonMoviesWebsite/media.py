9fe891c40bd98d37c3c6bdfe8a202166dd0f087b
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-16 02:08:15 +0530

    Removed a commented line

--- PythonMoviesWebsite/media.py
+++ PythonMoviesWebsite/media.py
@@ -5,8 +5,7 @@ class Movie():
     
     VALID_RATINGS = ["G","PG","PG-13","R"]
     
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
