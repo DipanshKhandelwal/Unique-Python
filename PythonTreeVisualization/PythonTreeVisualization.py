eaf5a875465982716694153128c65a1563fedb68
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-09-24 16:15:01 +0530

    Added PythonTreeVisualization

---
+++ PythonTreeVisualization/PythonTreeVisualization.py
@@ -0,0 +1,37 @@
+from sklearn.datasets import load_iris
+import numpy as np
+from sklearn import tree
+
+iris = load_iris()
+test_idx = [0, 50, 100]
+
+# training data
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
+
+# testing data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+print test_target
+print clf.predict(test_data)
+
+# tree visualization
+from sklearn.externals.six import StringIO
+import pydot
+dot_data = StringIO()
+tree.export_graphviz(clf,
+						out_file = dot_data,
+						feature_names = iris.feature_names,
+						class_names = iris.target_names,
+						filled = True, rounded = True,
+						impurity = False)
+
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
+
+print test_data[1], test_target[1]
+print iris.feature_names, iris.target_names
