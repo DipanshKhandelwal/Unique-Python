3e513c7543c348c3c77995eb1b519d2c16707e78
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-09-24 16:25:19 +0530

    Added axis-labels and title

--- PythonHistograms/PythonHistograms.py
+++ PythonHistograms/PythonHistograms.py
@@ -8,4 +8,7 @@ grey_height = 28 + 4 * np.random.randn(greyhounds)
 lab_height = 24 + 4 * np.random.randn(labs)
 
 plt.hist([grey_height, lab_height], stacked = True, color = ['r', 'b'])
+plt.title("Dogs Study")
+plt.ylabel("# of dogs")
+plt.xlabel("height of dogs")
 plt.show()
