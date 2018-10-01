8013914a01b0b91c0c6c86defb68136efbf2ebd8
Ankit Raj <ankitraj7994@gmail.com>
2018-10-01 20:12:00 +0530

    GetFiles.py debugged

--- Some basic programs/GetFiles.py
+++ Some basic programs/GetFiles.py
@@ -2,7 +2,12 @@ import os
 
 #This program finds all the files in a directory whose address we provide and prints it.
 def get_files():
-    file_list = os.listdir(r"C:\Users\DIPANSH KHANDELWAL\Desktop")
-    print (file_list)
+	directory = os.path.join('C:\\Users\\%s\\Desktop' %os.getlogin())
+	file_list = os.listdir(directory)
+	print (file_list)
 
-get_files()
+def main():
+	get_files()
+
+if __name__ == '__main__':
+	main()
